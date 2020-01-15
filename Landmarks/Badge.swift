7d1bb6b819127c3c4b5e3bebfe4f914d13e326cc
shuai.wang <624412421@qq.com>
2020-01-15 17:33:25 +0800

    add badge

---
+++ Landmarks/Badge.swift
@@ -0,0 +1,54 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by shuai.wang on 2020/1/15.
+//  Copyright © 2020 Thoughtworks. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        
+            GeometryReader { geometry in
+                Path { path in
+                    var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                    let height = width
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.95,
+                            y: height * (0.20 + HexagonParameters.adjustment)
+                        )
+                    )
+                    
+                    HexagonParameters.points.forEach {
+                        path.addLine(
+                            to: .init(
+                                x: width * $0.useWidth.0 * $0.xFactors.0,
+                                y: height * $0.useHeight.0 * $0.yFactors.0
+                            )
+                        )
+                        
+                        path.addQuadCurve(
+                            to: .init(
+                                x: width * $0.useWidth.1 * $0.xFactors.1,
+                                y: height * $0.useHeight.1 * $0.yFactors.1
+                            ),
+                            control: .init(
+                                x: width * $0.useWidth.2 * $0.xFactors.2,
+                                y: height * $0.useHeight.2 * $0.yFactors.2
+                            )
+                        )
+                    }
+                }
+                .fill(Color.black)
+            }
+        }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}

afb10faaaafcaabc7364f2617a7c086460e7b081
shuai.wang <624412421@qq.com>
2020-01-15 10:54:51 +0800

    add list

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -10,11 +10,35 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
     }
 }
 
-struct LandmarkDetail_Previews: PreviewProvider {
+struct LandmarkDetail_Preview: PreviewProvider {
     static var previews: some View {
         LandmarkDetail()
     }
--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -7,15 +7,42 @@
 //
 
 import SwiftUI
+import CoreLocation
 
-struct Landmark: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Landmark: Hashable, Codable, Identifiable {
+    
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+    
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+    
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
     }
 }
 
-struct Landmark_Previews: PreviewProvider {
-    static var previews: some View {
-        Landmark()
-    }
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
 }
+
+

a4c19b3603715d287029702555dd76218b83ded8
shuai.wang <624412421@qq.com>
2020-01-15 10:54:35 +0800

    add row

--- Landmarks/LandmarkRow.swift
+++ Landmarks/LandmarkRow.swift
@@ -9,13 +9,25 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow()
+        Group{
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
